f2c4a98e016c0f68332f178e1e064f6bb955f70d
HyeongGeunPark <hgpark677@gmail.com>
2023-10-24 19:18:39 +0900

    1697 solved

--- mycodebase.c
+++ mycodebase.c
@@ -835,6 +835,84 @@ static inline int __mm_heap_del(struct mm_heap *h, int minmax){
 #define min_heap_del(h) __mm_heap_del(h, 0)
 
 /*----------------------------------------------------------------------------*/
+
+// resizable circular queue
+
+#define QUEUE_SIZE 10000
+
+struct queue{
+    int *data;
+    int size;
+    int f;
+    int r;
+};
+
+#define queue_init(name) struct queue name={\
+        .data=malloc(sizeof(int)*(QUEUE_SIZE+1)), .size=QUEUE_SIZE,\
+        .f=0, .r=0}
+
+#define queue_free(name) free(name.data)
+
+static inline int queue_is_empty(struct queue *q){
+    return (q->f==q->r);
+}
+
+static inline int queue_is_full(struct queue *q){
+    int n = (q->r-q->f);
+    return n==1 || n==-q->size;
+}
+
+static inline void queue_resize(struct queue *q){
+    if(q->f>q->r){
+        q->size += QUEUE_SIZE;
+        q->data = realloc(q->data, sizeof(int)*(q->size+1));
+        return;
+    }
+    else{
+        int *dest = malloc(sizeof(int)*(q->size+QUEUE_SIZE+1));
+        int to_end = (q->size+1)-(q->r);
+        memcpy(dest, q->data+q->r, sizeof(int)*to_end);
+        memcpy(dest + to_end, q->data, sizeof(int)*q->f);
+        free(q->data);
+        q->data = dest;
+        q->r = 0;
+        q->f = q->size;
+        q->size += QUEUE_SIZE;
+    }
+    return;
+}
+
+static inline void queue_add_last(struct queue *q, int n){
+    if(queue_is_full(q)){
+        queue_resize(q);
+    }
+
+    q->data[q->f] = n;
+
+    if(q->f==q->size){
+        q->f=0;
+    }
+    else{
+        q->f++;
+    }
+}
+
+static inline int queue_del(struct queue *q){
+    if(queue_is_empty(q)){
+        printf("queue error: queue is empty\n");
+        return 0;
+    }
+    int r = q->data[q->r];
+    if(q->r==q->size){
+        q->r=0;
+    }
+    else{
+        q->r++;
+    }
+    return r;
+}
+
+/*----------------------------------------------------------------------------*/
 /*
 // wrapper of strcmp for qsort
 int mystrcmp(const void *a, const void *b){
