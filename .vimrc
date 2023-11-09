0f86d0ed439a8f0eeece2745dfcc7db4c4252f83
HyeongGeunPark <hgpark677@gmail.com>
2023-11-10 02:25:07 +0900

    14502 solving

---
+++ .vimrc
@@ -0,0 +1,128 @@
+" Vim with all enhancements
+source $VIMRUNTIME/vimrc_example.vim
+
+" Use the internal diff if available.
+" Otherwise use the special 'diffexpr' for Windows.
+if &diffopt !~# 'internal'
+  set diffexpr=MyDiff()
+endif
+function MyDiff()
+  let opt = '-a --binary '
+  if &diffopt =~ 'icase' | let opt = opt . '-i ' | endif
+  if &diffopt =~ 'iwhite' | let opt = opt . '-b ' | endif
+  let arg1 = v:fname_in
+  if arg1 =~ ' ' | let arg1 = '"' . arg1 . '"' | endif
+  let arg1 = substitute(arg1, '!', '\!', 'g')
+  let arg2 = v:fname_new
+  if arg2 =~ ' ' | let arg2 = '"' . arg2 . '"' | endif
+  let arg2 = substitute(arg2, '!', '\!', 'g')
+  let arg3 = v:fname_out
+  if arg3 =~ ' ' | let arg3 = '"' . arg3 . '"' | endif
+  let arg3 = substitute(arg3, '!', '\!', 'g')
+  if $VIMRUNTIME =~ ' '
+    if &sh =~ '\<cmd'
+      if empty(&shellxquote)
+        let l:shxq_sav = ''
+        set shellxquote&
+      endif
+      let cmd = '"' . $VIMRUNTIME . '\diff"'
+    else
+      let cmd = substitute($VIMRUNTIME, ' ', '" ', '') . '\diff"'
+    endif
+  else
+    let cmd = $VIMRUNTIME . '\diff'
+  endif
+  let cmd = substitute(cmd, '!', '\!', 'g')
+  silent execute '!' . cmd . ' ' . opt . arg1 . ' ' . arg2 . ' > ' . arg3
+  if exists('l:shxq_sav')
+    let &shellxquote=l:shxq_sav
+  endif
+endfunction
+
+if has("syntax")
+	syntax on
+endif
+
+"set hlsearch "검색어 하이라이팅
+set nu "줄번호
+set autoindent "자동 들여쓰기
+set scrolloff=2
+set wildmode=longest,list
+set ts=4 "tab 크기 
+set sts=4 "st select
+set sw=1 " 스크롤바 너비
+set autowrite " 다른 파일로 넘어갈 때 자동 저장
+set autoread " 작업 중인 파일 외부에서 변경됬을 경우 자동으로 불러옴
+set cindent " C언어 자동 들여쓰기
+set bs=eol,start,indent
+set history=256
+set laststatus=2 " 상태바 표시 항상
+"set paste " 붙여넣기 계단현상 없애기
+set shiftwidth=4 " 자동 들여쓰기 너비 설정
+set showmatch " 일치하는 괄호 하이라이팅
+set smartcase " 검색시 대소문자 구별
+set smarttab
+"set smartindednt
+set softtabstop=4
+set tabstop=4
+set ruler " 현재 커서 위치 표시
+set incsearch
+set statusline=\ %<%l:%v\ [%P]%=%a\ %h%m%r\ %F\
+" 마지막으로 수정된 곳에 커서를 위치함
+au BufReadPost *
+\ if line("'\"") > 0 && line("'\"") <= line("$") |
+\ exe "norm g`\"" |
+\ endif
+" 파일 인코딩을 한국어로
+if $LANG[0]=='k' && $LANG[1]=='o'
+set fileencoding=korea
+endif
+" 구문 강조 사용
+if has("syntax")
+ syntax on
+endif
+" 컬러 스킴 사용
+colorscheme gruvbox
+
+" 렌더링, ligature 글꼴
+if has("gui_running")
+  set encoding=utf-8  
+
+  source $VIMRUNTIME/mswin.vim
+  behave mswin
+  
+  if has('win32')
+      set keymodel=startsel
+       
+      " set guifont=Source\ Code\ Pro:h16:cANSI:qDEFAULT
+      " set guifont=JetBrains\ Mono\ Regular:h18:cANSI:qDEFAULT
+      set guifont=D2Coding:h12:cHANGEUL:qDEFAULT     
+	  set renderoptions=type:directx,
+                        \gamma:1.0,
+                        \contrast:0.5,
+                        \level:1,
+                        \geom:1,
+                        \renmode:5,
+                        \taamode:1
+
+      " restore Ctrl-F to Page down
+      unmap <C-F>
+  elseif has('gui_macvim')  
+      set guifont=JetBrains\ Mono\ 16
+      set antialias
+  else
+      set guifont=JetBrains\ Mono\ 16
+  endif
+  " Turn off toolbar
+  set guioptions-=T
+  " Turn on menu
+  set guioptions+=m
+end
+
+" 한줄 주석 처리 /**/
+map // V:s/^/\/*/<cr>gv:s/$/*\//<cr>:noh<cr>
+map /; V:s/^\/\*//<cr>gv:s/\*\/$/<cr>:noh<cr>
+" 여러줄 주석 처리 /**/
+vmap // :s/^/\/*/<cr>gv:s/$/*\//<cr>:noh<cr>
+vmap /; :s/^\/\*//<cr>gv:s/\*\/$/<cr>:noh<cr>
+
--- main.c
+++ main.c
@@ -52,113 +52,108 @@ static inline int readd(int *n){
     *n = sign?r:-r;
 }
 
-#define MOD 1000000007
+int map[8][8];
+int tmap[8][8];
+int virus[64][2];
+int space[64][2];
+int n, m;
 
-int gcd(int a, int b){
-    int r = a%b;
+int v_queue[100][2];
+int q_first;
+int q_last;
 
-    while(r){
-        a = b;
-        b = r;
-        r = a%b;
-    }
-    return b;
-}
-
-struct xgcd_r{
-    long long gcd;
-    long long s;
-    long long t;
-};
-
-struct xgcd_r xgcd(int a, int b){
-    struct xgcd_r res;
-
-    long long r, s1, s2, s, t1, t2, t, q;
-    s1 = 1;
-    s2 = 0;
-    t1 = 0;
-    t2 = 1;
-    while(1){
-        q = a/b;
-        r = a-(q*b);
-        s = s1-(q*s2);
-        t = t1-(q*t2);
-
-        if(r==0){
-            break;
-        }
-        a = b;
-        b = r;
-        s1 = s2;
-        s2 = s;
-        t1 = t2;
-        t2 = t;
-    }
-
-    res.gcd = b;
-    res.s = s2;
-    res.t = t2;
-
-    return res;
-}
-
-unsigned long long mod_inv(int x, int mod){
-    // 확장 유클리드 호제법
-    struct xgcd_r r = xgcd(x, mod);
-    unsigned long long res;
-    if(r.s<0){
-        res = (long long)r.s + (unsigned long long)MOD;
-    }
-    else{
-        res = (unsigned long long)r.s;
-    }
-    return res;
-}
-
-unsigned long long mod_power(int x, int pow, int mod){
-    unsigned long long result = 1;
-    unsigned long long temp = x;
-    while(pow){
-        if(pow&1){
-            result = (result * temp)%mod;
-        }
-        temp = (temp*temp)%mod;
-        pow>>=1;
-    }
-    return result;
-}
-
-unsigned long long mod_inv_fermi(int x, int mod){
-    // 페르마 소정리
-    // mod = prime number, x and mod are pairwise prime
-    return mod_power(x, mod-2, mod);
-}
 
 int main(void){
 
-
-    int m;
-    int i;
-    unsigned long long result = 0;
-    int n, s;
-    int g, si;
-    mymmap();
-
-    readd(&m);
-
-    for(i=0;i<m;i++){
-        readd(&n);
-        readd(&s);
-        g = gcd(n,s);
-        n/=g;
-        s/=g;
-        result = (result + (s*mod_inv_fermi(n,MOD)))%MOD;
+    int i, j, k;
+    int a, b, c;
+    int space_num = 0;
+    int virus_num = 0;
+    scanf("%d %d", &n, &m);
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            scanf("%d", &map[i][j]);
+            if(map[i][j]==0){
+                space[space_num][0] = i;
+                space[space_num][1] = j;
+                space_num++;
+            }
+            if(map[i][j]==2){
+                virus[virus_num][0] = i;
+                virus[virus_num][1] = j;
+                virus_num++;
+            }
+        }
     }
 
-    printf("%llu\n", result);
+    int max_safe = 0;
 
+    // wall select(brute force)
+    for(i=0;i<space_num;i++){
+        for(j=i+1;j<space_num;j++){
+            for(k=j+1;k<space_num;k++){
+                //selected wall -> space[i], space[j], space[k]
+                int sx[3] = {space[i][0], space[j][0], space[k][0]};
+                int sy[3] = {space[i][1], space[j][1], space[k][1]};
+                tmap[8][8]; //temporary map
+                memcpy(tmap, map, sizeof(int) * 64);
+                for(a=0;a<3;a++){
+                    tmap[sx[a]][sy[a]] = 1;  // make wall
+                }
 
+                // virus bfs
+                int safe_num = space_num-3;
+                q_first = q_last = 0;
+                for(a=0;a<virus_num;a++){
+                    v_queue[q_last][0] = virus[a][0];
+                    v_queue[q_last][1] = virus[a][1];
+                    q_last++;
+                }
+                while(q_last != q_first){
+                    int x, y, xx, yy;
+                    int dx[4] = {0, 0, 1, -1};
+                    int dy[4] = {1, -1, 0, 0};
+                    x = v_queue[q_first][0];
+                    y = v_queue[q_first][1];
+                    q_first++;
+                    for(b=0;b<4;b++){
+                        xx = x+dx[b];
+                        yy = y+dy[b];
+                        if( 0>xx || n<=xx) continue;    //boundary check
+                        if( 0>yy || m<=yy) continue;
+                        if(tmap[xx][yy]!=0) continue;   // visited check
+                        tmap[xx][yy] = 2;
+                        safe_num--;
+                        v_queue[q_last][0] = xx;
+                        v_queue[q_last][1] = yy;
+                        q_last++;
+                    }
+                }
+                if(safe_num > max_safe){
+                    max_safe = safe_num;
+                }
+            }
+        }
+    }
 
+    printf("%d\n", max_safe);
     return 0;
+}
+
+// gdb debug functions
+void tmapp(){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            printf("%2d ", tmap[i][j]);
+        }
+        printf("\r\n");
+    }
+}
+void mapp(){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            printf("%2d ", map[i][j]);
+        }
+        printf("\r\n");
+    }
 }
