f689514bc44f41ea02002163e477165b2e5c33e6
HyeongGeunPark <hgpark677@gmail.com>
2025-08-31 21:59:35 +0900

    backup : 250831

---
+++ bj/bj/6179.cpp
@@ -0,0 +1,73 @@
+/*
+
+bj6179 River Crossing
+
+John이 소(1<=N<=2500)를 데리고 강을 건넌다.
+
+뗏목을 타고 건너는데 걸리는 시간이 주어진다.
+
+혼자 건너갈 때 M(1<=M<=1000)분이 걸린다.
+여기에 소를 i마리 더 태우면 M_i(1<=M_i<=1000)분이 더 걸린다.
+참고로, i<j일때 M_i<M_j이다.
+
+John이 소와 함께 강을 건너는 데 걸리는 최소 시간을 구하시오.
+
+입력:
+N M
+M_1
+M_2
+...
+M_N
+
+접근: 동적 계획법.
+
+T_0 = M
+T_1 = M_1
+...
+T_N = min(
+	M_N,
+	T_(N-1) + T_0 + T_1,
+	T_(N-2) + T_0 + T_2,
+	...
+	T_(N-(N/2)) + T_0 + T_(N/2) <- integer division
+) 
+
+-> O(N^2)
+*/
+
+#include <iostream>
+//#include <memory>
+
+int main(void) {
+	// c++ fast io
+	std::cin.tie(0);
+	std::ios_base::sync_with_stdio(false);
+
+	int n;
+	std::cin >> n;
+
+	//std::unique_ptr<int[]> m_arr(new int[n+1]);
+	int* m_arr = new int[n + 1];
+
+	std::cin >> m_arr[0];
+
+	for (int i = 0; i < n; ++i) {
+		int temp;
+		std::cin >> temp;
+		m_arr[i + 1] = m_arr[i] + temp;
+	}
+
+	for (int i = 2; i <= n; ++i) {
+		for (int j = 1; j <= i / 2; ++j) {
+			m_arr[i] = std::min(
+				m_arr[i],
+				m_arr[i-j] + m_arr[0] + m_arr[j]
+				);
+		}
+	}
+
+	std::cout << m_arr[n];
+
+	delete[] m_arr;
+	return 0; 
+}
--- bj/src/main.cpp
+++ bj/src/main.cpp
@@ -1,49 +1,102 @@
 /*
+bj31876 자존심 강한 두 천재의 대결
 
-bj6179 River Crossing
+같은 길이의 U진법과 V진법으로 표기된 수의 대소를 비교하자.
 
-John이 소(1<=N<=2500)를 데리고 강을 건넌다.
+2 <= U, V <= 10이다.
+수의 길이는 최대 3'000'000이다.
 
-뗏목을 타고 건너는데 걸리는 시간이 주어진다.
+접근:
+unsigned long long으로 입력받을 수 있는 최대 숫자는 
+18'446'744'073'709'551'615
+즉, 10^19-1, 19자리까지는 안전하게 입력이 된다.
+ 9'999'999'999'999'999'999
 
-혼자 건너갈 때 M(1<=M<=1000)분이 걸린다.
-여기에 소를 i마리 더 태우면 M_i(1<=M_i<=1000)분이 더 걸린다.
-참고로, i<j일때 M_i<M_j이다.
+그런데 19자리의 가장 작은 10진수는 
+ 1'000'000'000'000'000'000 == 10^18
+19자리의 가장 큰 9진수는 9^19 - 1 == 
+ 1,350,851,717,672,992,088
 
-John이 소와 함께 강을 건너는 데 걸리는 최소 시간을 구하시오.
+20자리의 가장 작은 10진수는
+10'000'000'000'000'000'000 == 10^19
+20자리의 가장 큰 9진수는 9^20 - 1 ==
+12,157,665,459,056,928,800
 
-입력:
-N M
-M_1
-M_2
-...
-M_N
+21자리의 가장 작은 10진수는
+100'000'000'000'000'000'000 == 10^20
+20자리의 가장 큰 9진수는 9^21 - 1 ==
+109'418'989'131'512'359'208 
 
-접근: 
+22자리의 가장 작은 10진수는
+1'000'000'000'000'000'000'000 == 10^21
+22자리의 가장 큰 9진수는 9^22 - 1 ==
+  984'770'902'183'611'232'880
+
+  -> 22자리부터는 그냥 진법이 큰 쪽이 크다.
+
+어떻게든 10^23 범위가 표현 가능한 정수형이 있으면 쉽게 풀 수 있다.
+
+
+
+편법: 사실 임의 크기, 임의 진법 수의 입출력을 지원하는 언어/라이브러리를 사용하면
+바로 해결된다.
+
 */
 
 #include <iostream>
-#include <memory>
-//#include <vector>
+#include <cmath>
+#include <string>
 
 int main(void) {
 	// c++ fast io
-	std::cin.tie(0);
+	std::cin.tie(0); 
 	std::ios_base::sync_with_stdio(false);
 
-	int n, m;
-	std::cin >> n >> m;
+	int u, v;
+	std::cin >> u >> v;
+	std::string ras;
+	std::string auq;
 
-	std::unique_ptr<int[]> m_arr(new int[n]);
+	std::cin >> ras;
+	std::cin >> auq;
 
-	for (int i = 0; i < n; ++i) {
-		std::cin >> m_arr[i];
+	__int128
+
+	if (u == v) {
+		for (int i = 0; i < ras.length(); ++i) {
+			if (ras[i] > auq[i]) {
+				goto ras_won;
+			}
+			else if (ras[i] < auq[i]) {
+				goto auq_won;
+			}
+		}
+		goto tied; 
+	}
+	else if (u > v) {
+		double ras_log = 0.0l;
+		double auq_log = 0.0l;
+
+	}
+	else {
+
 	}
 
 
 
 
-	
+	std::cout << "something not good happened.";
+	return 0;
 
+ras_won:
+	std::cout << "ras";
+	return 0;
 
+auq_won:
+	std::cout << "auq";
+	return 0; 
+
+tied:
+	std::cout << "rasauq";
+	return 0;
 }
