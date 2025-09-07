3c764a47ba6a37a7f6d2cd7a023049e5c5db9d73
HyeongGeunPark <hgpark677@gmail.com>
2025-09-07 21:12:16 +0900

    backup: 250907, solving 14589

---
+++ bj/bj/14588.cpp
@@ -0,0 +1,145 @@
+/*
+bj14588 Line Friends (Small)
+
+수직선 위에 N개의 선분이 있고, 서로 겹치는 선분은 "친한" 선분이다.
+
+"친함" 관계로 그래프를 만들고, 각 선분 사이의 거리를 구하여라.
+
+선분 정보 입력 후 임의의 두 점 사이의 최단거리를 묻는 쿼리가 여러번 입력되면
+그에 알맞은 답을 출력하라.
+
+접근: 그래프 이론, 모든 점 사이의 최단거리 구하기 (플로이드-워셜)
+
+* 플로이드-워셜 알고리즘의 시간복잡도가 n^3이므로 선분 사이의 친함 관계를
+* 파악할 때 그냥 모든 선분 사이의 관계를 한 번씩 검토해 보는 것(n^2)으로 충분하다.
+*/
+
+#include<iostream>
+#include<vector>
+#include<cstring>
+#include<sstream>
+
+typedef std::pair<int, int> int_vec2; 
+
+class my_sstream : public std::basic_ostringstream<char>
+{
+public:
+	~my_sstream() {
+		std::cout << this->str();
+	}
+};
+
+my_sstream log_out;
+
+bool is_friend(const int_vec2& a, const int_vec2& b) {
+	//a   -------
+	//b           --------
+	if (a.second < b.first
+		// a               -------
+		// b        -----
+		|| b.second < a.first) return false;
+	else
+		return true;
+} 
+
+struct int_mat {
+public:
+	int_mat(const int row, const int col)
+		: row{ row }, col{ col }, data{ new int[col * row] }
+	{
+		std::memset(data, 0x00, sizeof(int) * col * row);
+	}
+
+	int& operator()(int _row, int _col) {
+		return data[_row * col + _col];
+	}
+ 
+	~int_mat() {
+		delete[] data;
+	}
+
+	const int row;
+	const int col;
+
+private:
+	int* data;
+
+};
+
+void debug_out(int_mat& obj) {
+
+	for (int i = 0; i < obj.row; ++i) {
+		for (int j = 0; j < obj.col; ++j) {
+			std::cerr << obj(i, j) << '\t';
+		}
+		std::cerr << '\n';
+	}
+}
+
+
+
+
+int main(void) {
+	// c++ fast io
+	std::cin.tie(0);
+	std::ios_base::sync_with_stdio(false);
+
+
+	// input
+	int n;
+	std::cin >> n; 
+
+	std::vector<int_vec2> lines(n); 
+
+	for (int i = 0; i < n; ++i) {
+		std::cin >> lines[i].first;
+		std::cin >> lines[i].second;
+	}
+
+	// build connectivity graph
+	int_mat g(n, n);
+
+	for (int i = 0; i < n; ++i) {
+		for (int j = i + 1; j < n; ++j) {
+			if (is_friend(lines[i], lines[j])) {
+				g(i, j) = g(j, i) = 1;
+			}
+			else {
+				// 적당히 크지만 오버플로우는 나지 않는 값
+				g(i, j) = g(j, i) = 1 << 20;
+			}
+		}
+	}
+
+
+	// floyd-warshall algorithm
+	for (int k = 0; k < n; ++k) {
+		for (int i = 0; i < n; ++i){
+			for (int j = 0; j < n; ++j) {
+				g(i, j) = std::min<int>(
+					g(i, j),
+					g(i,k) + g(k,j)
+					);
+			}
+		}
+	}
+
+	int q;
+	std::cin >> q;
+
+	for (int i = 0; i < q; ++i) {
+		int a, b;
+		std::cin >> a >> b;
+		// 1-based index to 0-based index
+		--a; --b;
+
+		if (g(a, b) < 1 << 20) {
+			log_out << g(a, b) << '\n'; 
+		}
+		else {
+			log_out << -1 << '\n';
+		}
+
+	} 
+	return 0; 
+}
--- bj/src/main.cpp
+++ bj/src/main.cpp
@@ -1,21 +1,138 @@
 /*
-bj14588 Line Friends (Small)
+bj14589 Line Friends (Large)
 
 수직선 위에 N개의 선분이 있고, 서로 겹치는 선분은 "친한" 선분이다.
 
 "친함" 관계로 그래프를 만들고, 각 선분 사이의 거리를 구하여라.
 
+2 <= N <= 150'000
+-1'000'000 <= L_i <= R_i <= 1'000'000
+
+1 <= Q <= 150'000 
+
 선분 정보 입력 후 임의의 두 점 사이의 최단거리를 묻는 쿼리가 여러번 입력되면
 그에 알맞은 답을 출력하라.
 
-접근: 그래프 이론, 모든 점 사이의 최단거리 구하기 (플로이드-워셜)
+접근
 
-* 플로이드-워셜 알고리즘의 시간복잡도가 n^3이므로 선분 사이의 친함 관계를
-* 파악할 때 그냥 모든 선분 사이의 관계를 한 번씩 검토해 보는 것(n^2)으로 충분하다.
+0. 계속된 쿼리에 대응하기 위해 어떤 식으로든 두 선분 사이의 거리를
+	구해 놓아야 한다.
+1. a -> b의 이동에서, 각 단계마다 가장 큰 끝점을 가진 선분으로 가는 것이
+	최적이다.
+2. 위 과정을 반복하면 거리를 구할 수 있지만, 이것을 모두 저장할 수는 없다.
+3. sparse table을 이용한다.
+	1. 임의의 선분 a에서, 2^i번(0<=i)의 이동으로 이동 가능한 최적의 선분을
+		찾아 저장해 둔다.
+	2. binary shifting을 이용해 a -> b의 최소 거리를 구한다.
+4. sparse table의 구축을 위해, 첫 항은 알아야 한다.
+	-> 이는 각 선분의 끝점보다 시작점이 작은 선분 중 끝점이 최대인 선분이다.  
+ 
 */
 
 #include<iostream>
+#include<vector>
+#include<sstream>
+#include<algorithm>
+
+
+struct Line {
+	int index, start, end;
+};
+
+class my_sstream : public std::basic_ostringstream<char>
+{
+public:
+	~my_sstream() {
+		std::cout << this->str();
+	}
+} log_out;
+
+
+bool is_friend(const Line& a, const Line& b) {
+	//a   -------
+	//b           --------
+	if (a.end < b.start
+		// a               -------
+		// b        -----
+		|| b.end < a.start) return false;
+	else
+		return true;
+} 
 
 int main(void) {
+	// c++ fast io
+	std::cin.tie(0);
+	std::ios_base::sync_with_stdio(false);
 
+
+	// input
+	int n;
+	std::cin >> n; 
+
+	std::vector<Line> lines(n); 
+
+	for (int i = 0; i < n; ++i) {
+		lines[i].index = i + 1;
+		std::cin >> lines[i].start;
+		std::cin >> lines[i].end;
+	}
+
+	// 끝점으로 정렬
+	std::sort(lines.begin(), lines.end(),
+		[](const Line& a, const Line& b)->bool {
+			if (a.start == b.start) {
+				return a.end < b.end;
+			}
+			else {
+				return a.start < b.start;
+			}
+		}); 
+
+
+
+	// 쿼리
+	int q;
+	std::cin >> q;
+
+	for (int i = 0; i < q; ++i) {
+		int a, b;
+		std::cin >> a >> b;
+		
+		a = index_transform[a];
+		b = index_transform[b];
+
+		if (a > b) {
+			std::swap(a, b);
+		}
+ 
+		int distance = 1;
+
+		while (true) {
+			if (is_friend(lines[a], lines[b])) {
+				log_out << distance << '\n';
+				break;
+			}
+			else {
+				auto it = std::upper_bound(
+					lines.begin(), 
+					lines.end(), 
+					Line{ 0, lines[a].end, 0 },
+					[](const Line& o1, const Line& o2)->bool {
+						return o1.start < o2.start;
+					}); 
+				--it;
+				int possible_next = std::distance(lines.begin(), it);
+
+				Line next = optimal_next[possible_next];
+				
+				if (lines[a].index == next.index) {
+					log_out << -1 << '\n';
+					break;
+				}
+				a = index_transform[next.index];
+				++distance;
+			}
+		} 
+	} 
+	return 0; 
 }
