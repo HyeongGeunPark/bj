50206cb9ffaa3f4a4629d2d8a719044f7e0a9632
HyeongGeunPark <94063825+HyeongGeunPark@users.noreply.github.com>
2025-12-19 17:53:18 +0900

    add: (251219, 박형근) 소스/헤더 템플릿 추가

---
+++ header_template.h
@@ -0,0 +1,56 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : header_template.h
+  * @brief          : c 헤더 파일 템플릿입니다.
+  * @author         :
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 BNT SYSTEM.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  * @details
+  * 단순한 헤더 파일 템플릿입니다. 복사해서 사용하십시오.
+  */
+
+/* USER CODE END Header */
+
+/* header guard */
+#ifndef __HEADER_TEMPLATE_H_
+#define __HEADER_TEMPLATE_H_
+
+/*****************************************************************/
+/************************ public includes ************************/
+// nothing exists
+
+
+/*****************************************************************/
+/************************* public macros *************************/
+// nothing exists
+
+/*****************************************************************/
+/******************** public types and enums *********************/
+// nothing exists
+
+
+/*****************************************************************/
+/*************** declaration of exported variables ***************/
+// nothing exists
+// e.g) extern int a;
+
+/*****************************************************************/
+/*********** definition of exported inline functions *************/
+// nothing exists
+
+
+/*****************************************************************/
+/************** declaration of exported functions ****************/
+// nothing exists
+
+#endif /* __HEADER_TEMPLATE_H_ */
---
+++ source_template.c
@@ -0,0 +1,80 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : source_template.c
+  * @brief          : c 소스 파일 템플릿입니다.
+  * @author         :
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 BNT SYSTEM.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  * @details
+  * 소스코드 템플릿입니다. 복사해서 사용하십시오.
+  *
+  *
+  */
+
+#include <misc/header_template.h>
+
+
+/*****************************************************************/
+/****************** private configuration macros *****************/
+// nothing exists
+
+
+/*****************************************************************/
+/************************* private includes **********************/
+// nothing exists
+
+
+/*****************************************************************/
+/************************* private macros ************************/
+// nothing exists
+
+
+/*****************************************************************/
+/******************** private types and enums ********************/
+// nothing exists
+
+
+/*****************************************************************/
+/************** definition of exported variables *****************/
+// nothing exists
+// e.g) int a = 0;
+
+
+/*****************************************************************/
+/********************* private variables *************************/
+// nothing exists
+
+
+/*****************************************************************/
+/************* forward declaration of private functions **********/
+// nothing exists
+
+
+/*****************************************************************/
+/********** declaration of exported inline functions *************/
+/* These are definitions in syntactic manner, refer the ISO C99 Standard. */
+// nothing exists
+
+
+/*****************************************************************/
+/**************** definition of exported functions ***************/
+// nothing exists
+
+
+/*****************************************************************/
+/********************** private functions ************************/
+// nothing exists
+
+
+
+/* USER CODE END Header */
